a16692f5da134f3e25a53b7a3597da23c4f7e557
VivekBaldha0712 <vivekbaldha712@gmail.com>
2023-07-18 11:31:05 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+
+export const About = () => {
+ 
+  return <div>This is about </div>
+};
+
+// export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import Addnote from './Addnote';
+import Notes from './Notes';
+
+export const Home = () => {
+
+    return (
+        <div>
+            
+
+            <Notes/>
+        </div>
+    )
+}
